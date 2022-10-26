d983e71c194e15070f96621558eda846c5723e46
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-10-26 10:05:47 +0530

    Add files via upload
    "[Pratu]"ADDED: Print The Table Of Power Of 2

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Power_Of_Two
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int power_2;
+            Console.WriteLine("Please enter value of n to print power of 2 table");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n >= 0 && n <= 30)
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    power_2 = Convert.ToInt32(Math.Pow(2, i));
+                    Console.WriteLine("2^" + i + " : " + power_2);
+                }
+            }
+            else
+               Console.WriteLine("Please Enter value of n from 0 to 30");
+        }
+    }
+}
+        
+    
+
